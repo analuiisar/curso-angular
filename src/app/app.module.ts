6f28180d6a34982d7dd6ac9616c288271022d4f4
Ana Luísa Ribeiro <analuiisar@gmail.com>
2025-08-24 15:31:44 -0300

    Criando componente serviços

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ServicosComponent } from './servicos/servicos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent
+    CadastroComponent,
+    ServicosComponent
   ],
   imports: [
     BrowserModule,
