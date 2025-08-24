393da91cc7d14db2a2a82b13573b1ea1803f0c1c
Ana Luísa Ribeiro <analuiisar@gmail.com>
2025-08-24 18:36:50 -0300

    adicionando imagem a header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import {MatButtonModule} from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
